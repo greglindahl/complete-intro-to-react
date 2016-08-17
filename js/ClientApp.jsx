893735bd8c7911ff8adde6e57e205a7e3d844ff1
greglindahl <greglindahl410@gmail.com>
2016-08-16 19:28:23 -0600

    Added standard and eslint as well as react router

---
+++ js/ClientApp.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Landing = require('./Landing')
+const { Router, Route, hashHistory } = require('react-router')
+
+const App = () => {
+  return (
+    <Router history={hashHistory}>
+      <Route path='/' component={Landing} />
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ js/Landing.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+
+const Landing = () => {
+  return (
+    <div className='app-container'>
+      <div className='home-info'>
+        <h1 className='title'>svideo</h1>
+        <input className='search' type='text' placeholder='Search' />
+        <button className='browse-all'> or Browse All</button>
+      </div>
+    </div>
+  )
+}
+
+module.exports = Landing
